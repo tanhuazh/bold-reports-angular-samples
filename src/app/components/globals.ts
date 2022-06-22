beadf925e98519e08144a688033176fe2685c792
Michael Tan <tanm@fcc.ca>
2022-06-22 19:16:15 -0400

    connect to local report service

--- src/app/components/globals.ts
+++ src/app/components/globals.ts
@@ -1,6 +1,6 @@
 export const Globals = {
-    SERVICE_URL: 'https://demos.boldreports.com/services/api/ReportViewerWebApi',
-    DESIGNER_SERVICE_URL: 'https://demos.boldreports.com/services/api/ReportDesignerWebApi',
+    SERVICE_URL: 'http://localhost:63779/api/ReportViewerWebApi',
+    DESIGNER_SERVICE_URL: 'http://localhost:63779/api/ReportDesignerWebApi',
     TOOLBAR_OPTIONS: {
         showToolbar: true,
         customGroups: [{
